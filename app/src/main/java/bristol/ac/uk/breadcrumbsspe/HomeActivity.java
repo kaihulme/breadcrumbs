4854eee22db07cae464e3f83f113675fa7cbe6d4
Antonio <ag17174@my.bristol.ac.uk>
2018-12-10 14:33:49 +0000

    QR code Reader implementation

--- app/src/main/java/bristol/ac/uk/breadcrumbsspe/HomeActivity.java
+++ app/src/main/java/bristol/ac/uk/breadcrumbsspe/HomeActivity.java
@@ -1,7 +1,12 @@
 package bristol.ac.uk.breadcrumbsspe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import bristol.ac.uk.breadcrumbsspe.qrcode.QRCodeCaptureActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        buttonClick();
+    }
+    private void buttonClick(){
+        Button scanButton = findViewById(R.id.scan_btn);
+        scanButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(HomeActivity.this, QRCodeCaptureActivity.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/bristol/ac/uk/breadcrumbsspe/QRCodeScannerActivity.java
@@ -0,0 +1,46 @@
+package bristol.ac.uk.breadcrumbsspe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.vision.barcode.Barcode;
+import bristol.ac.uk.breadcrumbsspe.qrcode.QRCodeCaptureActivity;
+
+public class QRCodeScannerActivity extends AppCompatActivity {
+    private TextView mResultTextView;
+    private static int QRCodeReaderRequestCode = 1;
+    private static String LogTag = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+        mResultTextView = findViewById(R.id.result_textview);
+
+        findViewById(R.id.scan_barcode_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), QRCodeCaptureActivity.class);
+                startActivityForResult(intent, QRCodeReaderRequestCode);
+            }
+        });
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        if(requestCode == QRCodeReaderRequestCode){
+            if(resultCode == CommonStatusCodes.SUCCESS){
+                if(intent != null){
+                    Barcode QRCode = intent.getParcelableExtra(QRCodeCaptureActivity.BarcodeObject);
+                    mResultTextView.setText(QRCode.displayValue);
+                }else
+                    mResultTextView.setText(R.string.no_qrcode_captured);
+            }else
+                Log.e(LogTag, String.format(getString(R.string.qrcode_error_format), CommonStatusCodes.getStatusCodeString(resultCode)));
+        }else
+            super.onActivityResult(requestCode, resultCode, intent);
+    }
+}
