9e5ca05ca14be9684c07b902b630fe6af24d1084
Kai Hulme <kaihulme97@gmail.com>
2019-03-18 11:58:17 +0000

    Extends imageController to get by id or name

--- web/src/main/java/com/spe/breadcrumbs/web/controller/ImageController.java
+++ web/src/main/java/com/spe/breadcrumbs/web/controller/ImageController.java
@@ -2,6 +2,7 @@ package com.spe.breadcrumbs.web.controller;
 
 import com.spe.breadcrumbs.dao.MapDAO;
 import com.spe.breadcrumbs.dao.MapDbDAO;
+import com.spe.breadcrumbs.entity.Map;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -21,13 +22,35 @@ public class ImageController {
 
     private MapDAO mapDAO = new MapDbDAO();
 
-    @RequestMapping(value = "/{id}", produces = MediaType.IMAGE_PNG_VALUE)
-    public ResponseEntity<byte[]> postImage(@PathVariable("id") Long id) throws SQLException {
-        Blob blob = mapDAO.getMap(id).getPicture();
-        byte[] picture = blob.getBytes(1, (int)blob.length());
-        final HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.IMAGE_PNG);
-        return new ResponseEntity<byte[]>(picture, headers, HttpStatus.OK);
+    @RequestMapping(value = "/{image}", produces = MediaType.IMAGE_PNG_VALUE)
+    public ResponseEntity<byte[]> postImage(@PathVariable("image") String image) throws SQLException {
+
+        Map map = mapDAO.getMapByName(image);
+        if (map != null) {
+            Blob blob = map.getPicture();
+            byte[] picture = blob.getBytes(1, (int)blob.length());
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.IMAGE_PNG);
+            return new ResponseEntity<byte[]>(picture, headers, HttpStatus.OK);
+        }
+
+        try {
+            Long id = (long) 0;
+            id = id.parseLong(image);
+            map = mapDAO.getMap(id);
+            if (map != null) {
+                Blob blob = map.getPicture();
+                byte[] picture = blob.getBytes(1, (int) blob.length());
+                final HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.IMAGE_PNG);
+                return new ResponseEntity<byte[]>(picture, headers, HttpStatus.OK);
+            }
+        } finally {
+            return new ResponseEntity<>(null, null, HttpStatus.NOT_FOUND);
+        }
+
     }
+
+
 
 }
