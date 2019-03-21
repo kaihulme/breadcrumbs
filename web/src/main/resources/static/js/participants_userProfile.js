8648b2a0fb028205353d9d76356ce8740b2add12
Kai Hulme <kaihulme97@gmail.com>
2019-03-21 19:51:47 +0000

    Removes redundant CSS and JavaScript

--- web/src/main/resources/static/js/participants_userProfile.js
+++
@@ -1,7 +0,0 @@
-// Jansy-Bootstrap add link to whole table row
-$('tbody.rowlink').rowlink()
-
-// navbar button functionality to toggle sidebar
-$('#sidebarCollapse').on('click', function () {
-    $('#sidebar, #content').toggleClass('active');
-});
