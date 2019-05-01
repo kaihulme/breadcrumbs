9cf9895020b96ffd407105fe8d7cb854a36405fd
M-Mafeni <mm17291@my.bristol.ac.uk>
2019-05-01 20:29:56 +0100

    implemented meeting api

--- web/src/main/java/com/spe/breadcrumbs/web/api/MeetingAPI.java
+++ web/src/main/java/com/spe/breadcrumbs/web/api/MeetingAPI.java
@@ -1,19 +1,26 @@
 package com.spe.breadcrumbs.web.api;
 
+import com.spe.breadcrumbs.dao.MeetingDAO;
+import com.spe.breadcrumbs.dao.MeetingDbDAO;
+import com.spe.breadcrumbs.entity.Meeting;
+import com.spe.breadcrumbs.web.DBConnection;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/api/meetings")
 
 public class MeetingAPI {
-    public ResponseEntity getMeeting(){
-        return null;
-    }
-    public ResponseEntity updateMeeting(){
-        return null;
+    private MeetingDAO meetingDAO = new MeetingDbDAO(new DBConnection());
+    @RequestMapping(method = RequestMethod.GET,value = "{userId}")
+    public ResponseEntity getMeeting(@PathVariable Long userId){
+        Meeting m = meetingDAO.getMeeting(userId);
+        if(m != null){
+            return new ResponseEntity<>(m, HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(m,HttpStatus.NOT_FOUND);
+        }
     }
 }
